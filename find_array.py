d7aaee49b55a3a488382d1714c2a4eb986eff6a6
Georgios Nikitopoulos <gnikitopoulos@inf.uth.gr>
2018-10-13 16:53:02 +0300

    Turn find_array.py into a module

--- find_array.py
+++ find_array.py
@@ -1,10 +1,13 @@
 import gdal
 
-nameraster = "/home/anon/Downloads/MOD10A1.A2018284.h18v05.006.2018286033502.hdf"
-hdl_file = gdal.Open(nameraster)
-subDatasets = hdl_file.GetSubDatasets()
-dataset = gdal.Open(subDatasets[0][0])
-geotransform = dataset.GetGeoTransform()
-band = dataset.GetRasterBand(1)
-array = band.ReadAsArray()
-print array
+def produce_array(tile_name):
+    nameraster = "/home/anon/Downloads/MOD10A1.A2018284.h18v05.006.2018286033502.hdf"
+    tile_name = nameraster
+    hdl_file = gdal.Open(tile_name)
+    subDatasets = hdl_file.GetSubDatasets()
+    dataset = gdal.Open(subDatasets[0][0])
+    geotransform = dataset.GetGeoTransform()
+    band = dataset.GetRasterBand(1)
+    array = band.ReadAsArray()
+    print array
+    return array
---
+++ latlong_transform.py
@@ -0,0 +1,4 @@
+import find_array
+
+my_array = find_array.produce_array("space")
+print my_array
